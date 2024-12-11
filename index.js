4de204caa6ae9ee9308ea07951aec6c650c1ebc7
lucas <lucas.caceres.lc@hotmail.com>
2024-12-11 19:53:43 -0300

    add json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let cart = JSON.parse(window.localStorage.getItem('cart')) || [];
 
 const getProducts = async () => {
    try {
-      const res = await fetch('/db.json');
+      const res = await fetch('https://lucas-caceres-w.github.io/Pre-entrega-talento-tech/db.json');
       const json = await res.json();
       products = json;
    } catch (error) {
